bfed312949dac86ba0b33cf1bff2acbf9bf27476
Kenneth Cao <AQ211003@aquinas.ac.uk>
2021-11-03 16:33:52 +0000

    Changed list to array and added extra bits

--- Homework Program/Program.cs
+++ Homework Program/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 namespace Homework_Program
 {
     class Program
@@ -10,16 +9,18 @@ namespace Homework_Program
             public string subject;
             public string description;
             public DateTime dueDate;
+            public bool complete;
         }
         static void Main(string[] args)
         {
-            List<homework> hmwk = new List<homework>();
-            gethomework(ref hmwk);
-            userselection(ref hmwk);
+            homework[] hmwk = new homework[20];
+            int homeworkcount = 0;
+            gethomework(ref hmwk, ref homeworkcount);
+            userselection(ref hmwk, ref homeworkcount);
             
         }
 
-        static void gethomework(ref List<homework> hmwk)
+        static void gethomework(ref homework[] hmwk, ref int homeworkcount)
         {
             StreamReader work = new StreamReader("homework.txt");
             while (!work.EndOfStream)
@@ -30,16 +31,19 @@ namespace Homework_Program
                 thishomework.subject = homework[0];
                 thishomework.description = homework[1];
                 thishomework.dueDate = DateTime.Parse(homework[2]);
+                thishomework.complete = bool.Parse(homework[3]);
+                hmwk[homeworkcount] = thishomework;
+                homeworkcount++;
 
-                hmwk.Add(thishomework);
-                
-               
-                
+
             }
             work.Close();
+            Console.WriteLine("{0} homework(s) loaded.\nPress any key to Continue.", homeworkcount);
+            Console.ReadKey();
+            Console.Clear();
         }
 
-        static void userselection(ref List<homework> hmwk)
+        static void userselection(ref homework[] hmwk, ref int homeworkcount)
         {
             
             string choice = "";
@@ -54,17 +58,17 @@ namespace Homework_Program
                 switch (choice)
                 {
                     case "1":
-                        viewhmwk(hmwk);
-                        userselection(ref hmwk);
+                        viewhmwk(hmwk, homeworkcount);
+                        userselection(ref hmwk, ref homeworkcount);
                         break;
                     case "2":
-                        completehmwk(ref hmwk);
+                        completehmwk(ref hmwk, ref homeworkcount);
                         break;
                     case "3":
-                        addhmwk(ref hmwk);
+                        addhmwk(ref hmwk, ref homeworkcount);
                         break;
                     case "4":
-                        outfile(hmwk);
+                        outfile(hmwk, homeworkcount);
                         Environment.Exit(0);
                         break;
                     default:
@@ -74,16 +78,20 @@ namespace Homework_Program
                 }
             }
         }
-        static void viewhmwk(List<homework> hmwk)
+        static void viewhmwk(homework[] hmwk, int homeworkcount)
         {
             Console.Clear();
             Console.WriteLine("=================================");
-            for (int i = 0; i < hmwk.Count; i++)
+            for (int i = 0; i < homeworkcount; i++)
             {
                 TimeSpan daysleft = hmwk[i].dueDate - DateTime.Today;
                 if (daysleft.Days <= 3)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
+                }
+                if (hmwk[i].complete == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
                 }
                 Console.WriteLine("Homework {0}:", i);
                 Console.WriteLine(hmwk[i].subject);
@@ -96,36 +104,42 @@ namespace Homework_Program
             
         }
 
-        static void completehmwk(ref List<homework> hmwk)
+        static void completehmwk(ref homework[] hmwk, ref int homeworkcount)
         {
-            viewhmwk(hmwk);
+            viewhmwk(hmwk, homeworkcount);
             Console.Write("What homework number would you like to complete: ");
             int choice = int.Parse(Console.ReadLine());
-            hmwk.RemoveAt(choice);
+            if (hmwk[choice].complete == true)
+            {
+                Console.WriteLine("This homework has already been completed.");
+                userselection(ref hmwk, ref homeworkcount);
+            }
+            hmwk[choice].complete = true;
             Console.Clear();
-            userselection(ref hmwk);
+            userselection(ref hmwk, ref homeworkcount);
 
         }
 
-        static void addhmwk(ref List<homework> hmwk)
+        static void addhmwk(ref homework[] hmwk, ref int homeworkcount)
         {
-            if(hmwk.Count >= 20)
+            if(homeworkcount >= 20)
             {
                 Console.WriteLine("The max limit of homework has been added.\nPlease complete some homework before adding more.");
-                userselection(ref hmwk);
+                userselection(ref hmwk, ref homeworkcount);
             }
+
             
             
-            homework thisHomework;
             Console.Write("Enter the subject of the homework: ");
-            thisHomework.subject = Console.ReadLine();
+            hmwk[homeworkcount].subject = Console.ReadLine();
             Console.Write("Enter a description of what the homework is about: ");
-            thisHomework.description = Console.ReadLine();
+            hmwk[homeworkcount].description = Console.ReadLine();
             DateTime date = DateTime.Today; 
             validateDate(ref date);
-            thisHomework.dueDate = date;
-            hmwk.Add(thisHomework);
-            userselection(ref hmwk);
+            hmwk[homeworkcount].dueDate = date;
+            hmwk[homeworkcount].complete = false;
+            homeworkcount++;
+            userselection(ref hmwk, ref homeworkcount);
             
 
 
@@ -154,12 +168,12 @@ namespace Homework_Program
             
         }
 
-        static void outfile(List<homework> hmwk)
+        static void outfile(homework[] hmwk, int homeworkcount)
         {
             StreamWriter writefile = new StreamWriter("homework.txt"); 
-            for (int i = 0; i < hmwk.Count; i++)
+            for (int i = 0; i < homeworkcount; i++)
             {
-                writefile.WriteLine("{0},{1},{2}", hmwk[i].subject, hmwk[i].description, hmwk[i].dueDate.ToString("dd/MM/yyyy"));
+                writefile.WriteLine("{0},{1},{2},{3}", hmwk[i].subject, hmwk[i].description, hmwk[i].dueDate.ToString("dd/MM/yyyy"),hmwk[i].complete.ToString());
 
             }
             writefile.Close();
